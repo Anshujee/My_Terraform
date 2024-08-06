ad915c641a8d624f7b6604ebcf89f6deaf1478b7
Anshu <jeeanshu24@gmail.com>
2024-08-06 09:15:29 +0530

    first terraform code to create EC2

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "First_ec2_by_using_terrraform" {
+    ami = "ami-0ad21ae1d0696ad58"
+    instance_type = "t2.micro"
+    # public_key= " "
+    #vpc_security_group_ids = ["vpc-0920e9e1d645c576e"]
+    subnet_id = "subnet-079be55a8cf563fe9"
+    key_name = "key_1_pair"
+    #count = 1
+    tags = {
+        Name= "Anshu_EC2"
+    }
+  
+}
